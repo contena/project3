f228a4e0827802764812caba08f87a77b0755557
contena <nancontena@yahoo.co.jp>
2017-10-01 21:00:36 +0900

    fix

--- JS/recruit_start.js
+++ JS/recruit_start.js
@@ -1,11 +1,10 @@
 function start(){
   var title = $("#title").val();
   var content = $('#content').val();
-  console.log(content);
+
   var tags = [];
   $(".tag").each(function(){//タグ
     tags.push($(this).text());
-    console.log($(this).text());
   });
   $.ajax({
     type:"post",
@@ -17,18 +16,36 @@ function start(){
       },
     success:function(html){
       console.log("send");
-      // $("footer").append(html);
     }
   });
-  // $.post("recruit_start.php",{
-  //     'title':title,'content':content,'tag':tags
-  // },function(data) {
-  //   console.log("send");
-  // });
+  console.log("すたーと");
+}
+
+function checkText(){
+  $(".viewError").remove();
+  var title = $("#title").val();
+  var content = $('#content').val();
+  var titleBool = true;
+  var contentBool = true;
+  // var tagBool = true;
+  if(title == ""){
+    titleBool = false;
+    $(".inputTitle").after('<span class="viewError">空白です</span>');
+  }
+  if(content == ""){
+    contentBool = false;
+    $(".inputContent").after('<span class="viewError">空白です</span>');
+  }
+
+  if(titleBool && contentBool){
+    start();
+  }
+
 }
 
 function tagControll(){
   var tag = $(".addTag").val();
   console.log(tag);
   $(".tags > li").prepend('<ul class="added"><li class="tag">'+ tag +'</li><button class="deleteButton">削除</button></ul>');
+  $(".addTag").val("");
 }
